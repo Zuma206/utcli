ba9d593fe6fc317062f001204175355ea4569046
Zuma <zuma@zuma.eu.org>
2024-09-15 22:18:51 +0100

    Added delete command

--- src/main.ts
+++ src/main.ts
@@ -56,6 +56,29 @@ program
     hasMore && console.log("More available...\n");
   });
 
+program
+  .command("delete")
+  .description("Delete a list of files by key")
+  .argument("<keys...>", "The list of file keys to delete")
+  .action(async (keys: string[]) => {
+    const utapi = createUTApi();
+    let n = 0;
+    keys.forEach(async (key) => {
+      const result = await utapi.deleteFiles(key);
+      if (result.success && result.deletedCount) {
+        console.log(
+          `Successfully deleted ${JSON.stringify(key)} [${++n}/${
+            keys.length
+          }]\n`
+        );
+      } else {
+        console.error(
+          `Failed to delete ${JSON.stringify(key)} [${++n}/${keys.length}]`
+        );
+      }
+    });
+  });
+
 program.parse(process.argv);
 
 function createUTApi() {
